1a4c2e6c738673261fc720a3e5868a5791435d22
Less Wright <lessw@etrillium.com>
2024-08-24 12:53:26 -0700

    Create setup.py

---
+++ extension/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name='bfloat16_compression',
+    ext_modules=[
+        CUDAExtension('bfloat16_compression', [
+            'bfloat16_compression.cu',
+        ], 
+        extra_compile_args={
+            'cxx': ['-O3'],
+            'nvcc': ['-O3', '-arch=sm_80']  # Adjust sm_XX based on your GPU architecture
+        })
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    }
+)
